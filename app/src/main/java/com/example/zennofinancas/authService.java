eaf89e882bc971a6b8a011bd13cce65b2a72f478
Vinicius <vnsxarts@gmail.com>
2025-08-27 16:05:23 -0300

    Validação da API

--- app/src/main/java/com/example/zennofinancas/authService.java
+++ app/src/main/java/com/example/zennofinancas/authService.java
@@ -1,22 +1,16 @@
 package com.example.zennofinancas;
 import com.google.gson.JsonObject;
-
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
-public class authService {
-    @Headers(
-            {
-                    "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkc3V2bGFlZXB3anpxbmZ2eHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNTMwMTIsImV4cCI6MjA3MTcyOTAxMn0.iuOiaoqm3BhPyEMs6mtn2KlA2CIuYdnkcfmc36_Z8t8",
-                    "Content-Type: application/json"
-            }
-    )
-    @POST("auth/v1/signup")
-    Call<JsonObject> register(@Body JsonObject body) {
-        return null;
-    }
+
+public interface authService {
+
+    @Headers({
+            "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtkc3V2bGFlZXB3anpxbmZ2eHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNTMwMTIsImV4cCI6MjA3MTcyOTAxMn0.iuOiaoqm3BhPyEMs6mtn2KlA2CIuYdnkcfmc36_Z8t8",
+            "Content-Type: application/json"
+    })
+    @POST("auth/v1/usuarios")
+    Call<JsonObject> register(@Body JsonObject body);
 }
-
-
-
