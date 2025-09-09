cde9d03601faf88f4e8372fe4666dc1014484348
Pedro <pedrocpereira16@gmail.com>
2025-09-09 15:15:13 -0300

    Criação de telas + correção de strings

--- app/src/main/java/com/example/zennofinancas/TelaEsqueceuASenha.java
+++ app/src/main/java/com/example/zennofinancas/TelaEsqueceuASenha.java
@@ -2,13 +2,33 @@ package com.example.zennofinancas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TelaEsqueceuASenha extends ActivityBase {
+public class TelaEsqueceuASenha extends ActivityBase
+{
+
+    Button btnEsqSenha;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_esqueceu_asenha);
+
+        btnEsqSenha = (Button) findViewById(R.id.btnEsqSenha);
+
+        btnEsqSenha.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent it = new Intent(TelaEsqueceuASenha.this,
+                        TelaChecarCodigo.class);
+                startActivity(it);
+            }
+        });
     }
 }
